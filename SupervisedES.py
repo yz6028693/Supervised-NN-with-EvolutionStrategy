fd8e7154a43c03fea3b1a06b663a5d83c14d5515
yz6028693 <zyuan6028693@gmail.com>
2017-12-17 14:12:42 -0600

    Update SupervisedES.py

--- SupervisedES.py
+++ SupervisedES.py
@@ -1,6 +1,6 @@
 '''
 In this demo, I will classify the same Archimedean Spiral pattern based on two categories of points we inputted
-in the Neural Network. For each point, 7 value of this point are inputted in the NN: x, y, x^2, y^2, xy, sin(x) and sin(y).
+in the Neural Network. 7 values of each point are inputted in the NN: x, y, x^2, y^2, xy, sin(x) and sin(y).
 This time, rather than use TensorFlow and Stochastic gradient descent (SGD), I will create this NN by myself and use
 Evolution Strategy to train this neural network.
 
